3fef0686ec342747c01d19ff8e54f1d8d1a079f2
ItsJiran <its.gilangramadhan@gmail.com>
2024-10-26 13:19:52 +0700

    add

--- frontend/pages/dashboard/index.vue
+++ frontend/pages/dashboard/index.vue
@@ -3,12 +3,19 @@
         page-name="infrastructure-dashboard"
         page-title="Untitled"
     >
+    <template v-slot:toolbar>
+        <v-btn icon>
+            <v-icon>
+                home
+            </v-icon>
+        </v-btn>
+    </template>
         <template v-slot:default></template>
     </page-home>
 </template>
 
 <script>
-export default {
-    name: "infrastructure-dashboard",
-};
+    export default {
+        name: "infrastructure-dashboard",
+    };
 </script>
