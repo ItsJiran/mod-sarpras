df66091291ff0ae88e71feb793a9b834c0003cc2
ItsJiran <its.gilangramadhan@gmail.com>
2024-11-12 05:39:10 +0700

    add

--- database/migrations/2024_09_21_151855_create_infrastructure_asset_lands.php
+++ database/migrations/2024_09_21_151855_create_infrastructure_asset_lands.php
@@ -16,6 +16,19 @@ return new class extends Migration
             $table->timestamp('receive_date')->nullable();
             $table->double('receive_price')->default(0);            
             $table->string('status')->default('tersedia');            
+
+            $table->string('atas_nama')->default('-');
+            $table->string('nop')->default('-');
+
+            $table->integer('luas_bumi')->unsigned()->nullable()->default(0);
+            $table->integer('luas_bangunan')->unsigned()->nullable()->default(0);
+
+            $table->integer('njop_bumi')->unsigned()->nullable()->default(0);
+            $table->integer('njop_bangunan')->unsigned()->nullable()->default(0);
+
+            $table->string('alamat_wajib_pajak')->default('-');
+            $table->string('alamat_objek_pajak')->default('-');
+
             $table->jsonb('meta')->nullable();
             $table->softDeletes();
             $table->timestamps();
